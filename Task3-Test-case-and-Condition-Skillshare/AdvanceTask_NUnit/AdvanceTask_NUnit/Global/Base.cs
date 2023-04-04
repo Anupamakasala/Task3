aa9d2768007c64b80410da168f28a42fe9545eaf
spriti thakur <thakurspriti003@gmail.com>
2023-04-04 13:44:30 +1200

    Automation task

---
+++ Task3-Test-case-and-Condition-Skillshare/AdvanceTask_NUnit/AdvanceTask_NUnit/Global/Base.cs
@@ -0,0 +1,95 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using AdvanceTask_NUnit.Pages;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdvanceTask_NUnit.Global.GlobalDefinitions;
+
+
+
+namespace AdvanceTask_NUnit.Global
+{
+    public class Base
+    {
+        public static IWebDriver driver;
+        #region
+        // Define other objects here
+
+        public static ExtentReports extentReportObj = null;
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentTest test;
+
+        static string excelPath = @"C:\Users\Admin\Downloads\Task3-Test-case-and-Condition-Skillshare\Task3-Test-case-and-Condition-Skillshare\AdvanceTask_NUnit\AdvanceTask_NUnit\TestData\TestData.xlsx";
+        static string reportPath = @"C:\Users\Admin\Downloads\Task3-Test-case-and-Condition-Skillshare\Task3-Test-case-and-Condition-Skillshare\AdvanceTask_NUnit\AdvanceTask_NUnit\TestData\TestReports";
+        static string screenshotsPath = @"C:\Users\Admin\Downloads\Task3-Test-case-and-Condition-Skillshare\Task3-Test-case-and-Condition-Skillshare\AdvanceTask_NUnit\AdvanceTask_NUnit\TestData\Screenshots";
+
+        #endregion
+
+        public static string ExcelPath { get => excelPath; set => excelPath = value; }
+   
+
+        [OneTimeSetUp]
+        public void LoginActions()
+        {
+
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(reportPath);
+            extentReportObj = new ExtentReports();
+            extentReportObj.AttachReporter(htmlReporter);
+
+
+        }
+
+        [SetUp]
+        public void Inititalize()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5000");
+            driver.Manage().Window.Maximize();
+
+
+            //Load Excel
+            
+           ExcelUtil.PopulateInCollection(Base.excelPath, "LoginPage");
+
+            //Open URL
+            driver.Navigate().GoToUrl(ExcelUtil.ReadData(2, "Url"));
+
+        }
+
+
+        public class GetScreenShot
+        {
+            public static string Capture(IWebDriver driver, string screenShotName)
+            {
+                ITakesScreenshot ts = (ITakesScreenshot)driver;
+                Screenshot screenshot = ts.GetScreenshot();
+                string pth = System.Reflection.Assembly.GetCallingAssembly().Location;
+                string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "Screenshots\\" + screenShotName + ".png";
+                string localpath = new Uri(finalpth).LocalPath;
+                screenshot.SaveAsFile(localpath, ScreenshotImageFormat.Png);
+                return localpath;
+            }
+        }
+
+
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+
+            extentReportObj.Flush();
+           // driver.Quit();
+
+        }
+
+    }
+}
---
+++ Task3-Test-case-and-Condition-Skillshare/AdvanceTask_NUnit/AdvanceTask_NUnit/Tests/Tests.cs
@@ -0,0 +1,64 @@
+﻿using AdvanceTask_NUnit.Global;
+using AdvanceTask_NUnit.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdvanceTask_NUnit.Tests
+{
+
+    [TestFixture]
+    [Parallelizable]
+    public class Tests:Base
+    {
+        SignUp signUpObj;
+        LoginPage loginPageObj;
+
+        public Tests()
+        {
+            signUpObj = new SignUp();
+            loginPageObj = new LoginPage();
+        }
+
+
+        [Test, Order(1), Description("Signup for Portal")]
+        public void Register()
+        {
+            try
+            {
+                test = extentReportObj.CreateTest("SignUp for Portal Passed");
+                signUpObj.Register();
+                Thread.Sleep(2000);
+
+            }
+            catch (NoSuchElementException e)
+            {
+                test.Fail(e.StackTrace);
+            }
+
+        }
+
+            [Test, Order(2), Description("SignIn for the Portal")]
+            public void LogininSteps()
+        {
+            try
+            {
+                test = extentReportObj.CreateTest("SignIn for the Portal passed");
+                loginPageObj.LoginSteps();
+                Thread.Sleep(2000);
+
+            }
+            catch (NoSuchElementException e)
+            {
+                test.Fail(e.StackTrace);
+            }
+        }
+        }
+
+    }
+
