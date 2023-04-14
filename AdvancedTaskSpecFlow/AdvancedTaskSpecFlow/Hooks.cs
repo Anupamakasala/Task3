d5191fe0a2a78111f9bd36d281691474a094e393
BindyaB <bindbindya@yahoo.co.in>
2023-04-14 19:30:05 +1200

    Framework for Specflow

---
+++ AdvancedTaskSpecFlow/AdvancedTaskSpecFlow/Hooks.cs
@@ -0,0 +1,31 @@
+﻿using AdvancedTaskSpecFlow.Pages;
+using AdvancedTaskSpecFlow.Utilities;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace AdvancedTaskSpecFlow
+{
+    [Binding]
+    public class Hooks:CommonDriver
+    {
+        [BeforeTestRun]
+        public void login()
+        {
+            driver = new ChromeDriver();
+            LoginPage loginPageObj = new LoginPage();
+        }
+
+
+
+        [AfterTestRun]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
