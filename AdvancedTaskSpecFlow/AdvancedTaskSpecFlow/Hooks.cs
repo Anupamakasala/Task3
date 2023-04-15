2dc52366e9ae63a077f660e0f5f01205125f788c
BindyaB <bindbindya@yahoo.co.in>
2023-04-16 11:35:01 +1200

    Added Dummy Feature file and Step Definition

--- AdvancedTaskSpecFlow/AdvancedTaskSpecFlow/Hooks.cs
+++ AdvancedTaskSpecFlow/AdvancedTaskSpecFlow/Hooks.cs
@@ -1,20 +1,17 @@
 ﻿using AdvancedTaskSpecFlow.Pages;
 using AdvancedTaskSpecFlow.Utilities;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace AdvancedTaskSpecFlow
 {
     [Binding]
-    public class Hooks:CommonDriver
+    public sealed class Hooks:CommonDriver
     {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
         [BeforeTestRun]
-        public void login()
+        public static void login()
         {
             driver = new ChromeDriver();
             LoginPage loginPageObj = new LoginPage();
@@ -23,9 +20,33 @@ namespace AdvancedTaskSpecFlow
 
 
         [AfterTestRun]
-        public void CloseBrowser()
+        public static void CloseBrowser()
         {
             driver.Close();
+        }
+
+        [BeforeScenario("@tag1")]
+        public static void BeforeScenarioWithTag()
+        {
+            // Example of filtering hooks using tags. (in this case, this 'before scenario' hook will execute if the feature/scenario contains the tag '@tag1')
+            // See https://docs.specflow.org/projects/specflow/en/latest/Bindings/Hooks.html?highlight=hooks#tag-scoping
+
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [BeforeScenario(Order = 1)]
+        public static void FirstBeforeScenario()
+        {
+            // Example of ordering the execution of hooks
+            // See https://docs.specflow.org/projects/specflow/en/latest/Bindings/Hooks.html?highlight=order#hook-execution-order
+
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [AfterScenario]
+        public static void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
         }
     }
 }
