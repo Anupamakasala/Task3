6c564856b04991e47413d41da01fa19bf5bcfa03
spriti thakur <thakurspriti003@gmail.com>
2023-04-19 11:50:31 +1200

    Commit

---
+++ AdvanceTask_NUnit/AdvanceTask_Nunit/Pages/LoginPage.cs
@@ -0,0 +1,52 @@
+﻿using AdvanceTask_NUnit.Global;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdvanceTask_NUnit.Global.GlobalDefinitions;
+
+namespace AdvanceTask_NUnit.Pages
+{
+    public class LoginPage : Base
+    {
+
+
+        private IWebElement SignIn => driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div[1]/div/a"));
+
+        // Finding the Email Field
+        private IWebElement Email => driver.FindElement(By.Name("email"));
+
+        //Finding the Password Field
+        private IWebElement Password => driver.FindElement(By.Name("password"));
+
+        //Finding the Login Button
+        private IWebElement LoginBtn => driver.FindElement(By.XPath("//button[contains(text(),'Login')]"));
+
+     
+        public void LoginSteps()
+        {
+            //Populate excel data
+
+            ExcelUtil.PopulateInCollection(Base.ExcelPath, "LoginPage");
+
+            //Click on Signin button
+            Thread.Sleep(3000);
+            SignIn.Click();
+
+            //Enter email
+            Email.SendKeys(ExcelUtil.ReadData(2, "Username"));
+
+            //Enter password
+            Password.SendKeys(ExcelUtil.ReadData(2, "Password"));
+
+            //Click Login button
+            LoginBtn.Click();
+            //wait(2);
+
+            }
+          
+        }
+    }
